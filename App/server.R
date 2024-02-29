0ce4f8c0ae01390faa69cb849d795178ed1a793e
Rowan121 <155587723+Rowan121@users.noreply.github.com>
2024-02-29 02:00:15 -0800

    Updated server for possible third visualizationR

--- App/server.R
+++ App/server.R
@@ -13,6 +13,16 @@ server <- function(input, output){
         
       return(viz_1_output)   
   })
-  
-  
+
+  output$viz_3_output <- renderPlotly({
+    filtered_df <- df %>%
+      filter(.data[[input$user_selection_viz_3_1]] == input$user_selection_viz_3_2)
+
+    viz_3_output <- ggplot(filtered_df, aes(x = .data[[input$user_selection_viz_3_2]])) +
+      geom_bar() +
+      labs(title = "Mental and Social Health Factors", x = "Social Health Factor", y = "Count")
+
+    ggplotly(viz_3_output)
+  })
+
 }
