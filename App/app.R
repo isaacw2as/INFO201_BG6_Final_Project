f71f7e28589f7960294e49bae8e3c4de216e6181
Nicholas Wyatt <noahmega0@gmail.com>
2024-03-06 13:09:15 -0800

    Final Commit

--- App/app.R
+++ App/app.R
@@ -6,3 +6,6 @@ source("ui.R")
 source("server.R")
 
 shinyApp(ui = ui, server = server)
+
+# Shiny App Link:
+# https://readox.shinyapps.io/INFO_201_Final_Project/
---
+++ README.txt
@@ -0,0 +1,2 @@
+Shiny App Link:
+https://readox.shinyapps.io/INFO_201_Final_Project/
